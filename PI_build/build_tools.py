e28f129ca827884ff8b68753e20ccdc8147538f1
Jeremy Gill <jgill@parallax-innovations.com>
2013-06-21 11:57:06 -0400

    ENH: PEP8 cleanups

--- PI_build/build_tools.py
+++ PI_build/build_tools.py
@@ -5,6 +5,8 @@
 import os
 import re
 from dulwich.repo import Repo
+from dulwich.errors import NotGitRepository
+
 
 class PIVersionInfo(object):
     """Encapsulates a project's version info.
@@ -48,30 +50,33 @@ class PIVersionInfo(object):
 
        E-mail of the maintainer
     """
-    def __init__(self):
-    	"""Initialize the object with dummy values.
 
+    def __init__(self):
+        """Initialize the object with dummy values.
+
+        :rtype : None
         The default values are of limited value."""
-        self.VER_PRODUCT_MAJOR    = '0'
-        self.VER_PRODUCT_MINOR    = '0'
+        self.VER_PRODUCT_MAJOR = '0'
+        self.VER_PRODUCT_MINOR = '0'
         self.VER_PRODUCT_REVISION = '0'
-        self.VER_PRODUCT_BUILD    = '1'
-        self.SHORT_SHA1           = ''
-        self.FULL_VERSION         = ''
-        self.VERSION              = ''
-        self.MAINTAINER           = "Jeremy Gill"
-        self.MAINTAINER_EMAIL     = "jgill@parallax-innovations.com"
+        self.VER_PRODUCT_BUILD = '1'
+        self.SHORT_SHA1 = ''
+        self.FULL_VERSION = ''
+        self.VERSION = ''
+        self.MAINTAINER = "Jeremy Gill"
+        self.MAINTAINER_EMAIL = "jgill@parallax-innovations.com"
 
     def get_dictionary(self):
-    	"""Returns a dictionary of version info.
+        """Returns a dictionary of version info.
 
         Call this method if you need to perform string replacements.  The contents
         of the dictionary can be used for automatic string replacments, such as in
         the following example::
 
-		"full_version = %(FULL_VERSION)s" % build_tools.PIVersionInfo().get_dictionary()
+        "full_version = %(FULL_VERSION)s" % build_tools.PIVersionInfo().get_dictionary()
 
-	"""
+        :rtype: A dictionary of version strings
+        """
         return {
             'VER_PRODUCT_MAJOR': self.VER_PRODUCT_MAJOR,
             'VER_PRODUCT_MINOR': self.VER_PRODUCT_MINOR,
@@ -83,28 +88,29 @@ class PIVersionInfo(object):
             'MAINTAINER': self.MAINTAINER,
             'MAINTAINER_EMAIL': self.MAINTAINER_EMAIL,
         }
-        
-def get_version_strings(_dir):
 
-    '''
+
+def get_version_strings(_dir):
+    """
     A global function that returns a :class:`PIVersionInfo` object, encapsulating your project's version info.
 
     :param _dir: directory of the toplevel folder in your project.  :file:`.git` should be 
                  found immediately below this directory.
     :rtype: A :class:`PIVersionInfo` object.
-    '''
+    """
     info = PIVersionInfo()
+    r = None
 
     while True:
         try:
             r = Repo(_dir)
             break
-        except:
+        except NotGitRepository:
             _dir2 = os.path.abspath(os.path.join(_dir, '..'))
             if _dir != _dir2:
                 _dir = _dir2
             else:
-                raise "Unable to find git repository"
+                raise Exception("Unable to find git repository")
 
     info.PACKAGE_SHA1 = r.get_refs()['HEAD']
 
@@ -112,10 +118,11 @@ def get_version_strings(_dir):
     for entry in r.get_refs():
         if 'tags' in entry:
             sha1 = r.get_refs()[entry]
-            sha1_to_tag[sha1] = entry.replace('refs/tags/','')
+            sha1_to_tag[sha1] = entry.replace('refs/tags/', '')
 
     count = 0
     ex = re.compile('v[0-9]*\.[0-9]*\.[0-9]*$')
+    git_version = 'v1.0.0-g0000'
 
     for walker in r.get_walker():
         commit = walker.commit
@@ -126,8 +133,10 @@ def get_version_strings(_dir):
                 break
         count += 1
 
-    info.VER_PRODUCT_MAJOR, info.VER_PRODUCT_MINOR, info.VER_PRODUCT_REVISION, info.VER_PRODUCT_BUILD, info.SHORT_SHA1 = re.compile('v([0-9]*).([0-9]*).([0-9]*)-([0-9]*)-(.*)').match(git_version).groups()
+    info.VER_PRODUCT_MAJOR, info.VER_PRODUCT_MINOR, info.VER_PRODUCT_REVISION, info.VER_PRODUCT_BUILD, info.SHORT_SHA1 = re.compile(
+        'v([0-9]*).([0-9]*).([0-9]*)-([0-9]*)-(.*)').match(git_version).groups()
     info.FULL_VERSION = git_version
-    info.VERSION = '%s.%s.%s-%s' % (info.VER_PRODUCT_MAJOR, info.VER_PRODUCT_MINOR, info.VER_PRODUCT_REVISION, info.VER_PRODUCT_BUILD)
-    
+    info.VERSION = '%s.%s.%s-%s' % (info.VER_PRODUCT_MAJOR, info.VER_PRODUCT_MINOR, info.VER_PRODUCT_REVISION,
+                                    info.VER_PRODUCT_BUILD)
+
     return info
--- setup.py
+++ setup.py
@@ -2,18 +2,24 @@
 # Copyright (c) 2011-2013 Parallax Innovations Inc.
 # =========================================================================
 
+import os
 from distutils.core import setup
+from PI_build import build_tools
 
-setup(name                  = 'PI_build',
-      version               = '0.0.1',
-      license               = "Commercial",
-      description           = "Parallax Innovations Build scripts",
-      author                = "Jeremy D. Gill",
-      author_email          = "jgill@parallax-innovations.com",
-      maintainer            = "Jeremy D. Gill",
-      maintainer_email      = "jgill@parallax-innovations.com",
-      requires              = ['dulwich'],
-      url                   = "http://www.parallax-innovations.com",
-      packages              = ['PI_build'],
-      long_description      = "Parallax Innovations Build scripts",
-     )
+_dir = os.path.dirname(__file__)
+info = build_tools.get_version_strings(_dir)
+print info
+
+setup(name='PI_build',
+      version='0.0.1',
+      license="Commercial",
+      description="Parallax Innovations Build scripts",
+      author="Jeremy D. Gill",
+      author_email="jgill@parallax-innovations.com",
+      maintainer="Jeremy D. Gill",
+      maintainer_email="jgill@parallax-innovations.com",
+      requires=['dulwich'],
+      url="http://www.parallax-innovations.com",
+      packages=['PI_build'],
+      long_description="Parallax Innovations Build scripts",
+)
