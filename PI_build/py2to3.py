1a47190ae144b632743dcc2d9518483d34b78938
Jeremy Gill <jgill@parallax-innovations.com>
2017-01-17 15:08:17 -0500

    FIX: handle reminaing pyc files at top-most level

--- PI_build/py2to3.py
+++ PI_build/py2to3.py
@@ -6,18 +6,25 @@ from zipfile import ZipFile, ZIP_DEFLATED
 def rewrite_egg(egg_path):
     """Create an egg that works with MicroView in python3"""
 
-    RE_PYCACHE_FILE = re.compile('(.+/)__pycache__/(.+)\.cpython-3\d(.+)(.pyc|.pyo)$')
+    RE_PYCACHE_FILE = re.compile('(.*/)__pycache__/(.+)\.cpython-3\d(.+)(.pyc|.pyo)$')
+    RE_PYCACHE_FILE2 = re.compile('__pycache__/(.+)\.cpython-3\d(.+)(.pyc|.pyo)$')
 
     with tempfile.TemporaryDirectory() as tmpdir:
         names_lst = []
         tmpdir_abs_path = os.path.abspath(tmpdir)
         with ZipFile(egg_path, 'r') as zf:
             for name in zf.namelist():
-                m = RE_PYCACHE_FILE.match(name)
-                if m:
-                    w_name = m.group(1) + m.group(2) + m.group(4)
+
+                if '__pycache__' in name:
+                    if name.startswith('__pycache__'):
+                        m = RE_PYCACHE_FILE2.match(name)
+                        w_name = m.group(1) + m.group(3)
+                    else:
+                        m = RE_PYCACHE_FILE.match(name)
+                        w_name = m.group(1) + m.group(2) + m.group(4)
                 else:
                     w_name = name
+
                 w_path = os.path.join(tmpdir_abs_path, w_name)
                 dir_path = os.path.dirname(w_path)
                 if not os.path.exists(dir_path):
