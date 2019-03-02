a8157a47e3663ed54a06c0bf1c7fd605bbe6383f
Jeremy Gill <jgill@parallax-innovations.com>
2019-03-02 13:12:13 -0500

    ENH: integrate pytest more tightly

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@
 import os
 import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
 try:
     from pip._internal.req.req_file import parse_requirements
     from pip._internal.download import PipSession
@@ -18,9 +20,26 @@ version = os.popen('git describe --tags').read().strip().replace('v','')
 install_reqs = parse_requirements("requirements.txt", session=PipSession())
 reqs = [str(ir.req) for ir in install_reqs]
 
-# add a few more requirements
-if sys.platform == 'win32':
-    reqs.extend(['pefile'])
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(name='PI_build',
       version=version,
@@ -34,6 +53,7 @@ setup(name='PI_build',
       tests_require=['pytest', 'tempdir>=0.7.1', 'future'],
       install_requires=reqs,
       url="http://www.parallax-innovations.com",
+      cmdclass = {'test': PyTest},
       packages=['PI_build'],
       long_description="Parallax Innovations Build scripts",
 )
