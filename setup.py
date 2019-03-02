54bfde9d1b85d3b2cfbd5e2342d548d0b4cc5fea
Jeremy Gill <jgill@parallax-innovations.com>
2019-03-02 14:01:28 -0500

    ENH: integration using pytest-runner

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@
 import os
 import sys
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
 
 try:
     from pip._internal.req.req_file import parse_requirements
@@ -17,28 +16,13 @@ except:
 # do it using git here to avoid depending on dulwich at this point
 version = os.popen('git describe --tags').read().strip().replace('v','')
 
-install_reqs = parse_requirements("requirements.txt", session=PipSession())
-reqs = [str(ir.req) for ir in install_reqs]
+reqs = [
+    'dulwich',
+    'psutil'
+]
 
-
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
+if sys.platform == 'win32':
+    reqs.append('pefile')
 
 
 setup(name='PI_build',
@@ -49,11 +33,12 @@ setup(name='PI_build',
       author_email="jgill@parallax-innovations.com",
       maintainer="Jeremy D. Gill",
       maintainer_email="jgill@parallax-innovations.com",
+      setup_requires=['pytest-runner'],
       requires=reqs,
       tests_require=['pytest', 'tempdir>=0.7.1', 'future'],
       install_requires=reqs,
       url="http://www.parallax-innovations.com",
-      cmdclass = {'test': PyTest},
+      #cmdclass = {'test': PyTest},
       packages=['PI_build'],
       long_description="Parallax Innovations Build scripts",
 )
