ca229b1c7b9072e4632084a63ac6f1e209fb7822
Jeremy Gill <jgill@parallax-innovations.com>
2022-01-16 03:52:47 +0000

    ADD: add a nix builder

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ pkgs ? import <nixpkgs> {} }: 
+with pkgs.python39Packages;
+buildPythonPackage rec {
+  pname = "PI_build";
+  version = "0.1.3-51-g24831f9";
+
+  src = fetchGit {
+    url = "ssh://git@bitbucket.org/jgill72/PI_build.git";
+    rev = "24831f9e84f0d931fe8189088d70f9592a3f29c0";
+    allRefs = true;
+  };
+
+  nativeBuildInputs = [
+    pkgs.git
+  ];
+
+  doCheck = false;
+
+  buildInputs = [
+    dulwich
+    pip
+    psutil
+    setuptools
+    pkgs.git
+  ];
+
+  meta = with pkgs.lib; {
+    description = "A Python wrapper around the Twitter API";
+    homepage = "https://github.com/bear/python-twitter";
+    license = licenses.asl20;
+    maintainers = [ maintainers.marsam ];
+  };
+}
