3983b7637baea7c31094fc5694295e2adeb7876a
Jeremy Gill <jeremy.gill@gmail.com>
2025-05-31 18:36:44 -0400

    Update flake

--- flake.nix
+++ flake.nix
@@ -10,17 +10,44 @@
     in {
       packages.${system}.default = import ./default.nix { pkgs = pkgs; };
 
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.python3
-          pkgs.python3Packages.hatchling
-          pkgs.python3Packages.hatch-vcs
-          pkgs.python3Packages.dulwich
-          pkgs.python3Packages.psutil
-          pkgs.python3Packages.pytest
-          pkgs.python3Packages.pytest-cov
-          pkgs.python3Packages.pytest-asyncio
-        ];
+      # Define the development shell environment
+      # This will include Python, Hatchling, and other necessary packages
+      # for development and testing of the project
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python3
+            pkgs.python3Packages.hatchling
+            pkgs.python3Packages.hatch-vcs
+            pkgs.python3Packages.dulwich
+            pkgs.python3Packages.psutil
+            pkgs.python3Packages.pytest
+            pkgs.python3Packages.pytest-cov
+            pkgs.python3Packages.pytest-asyncio
+          ];
+        };
+
+        # Define a development shell for MkDocs with Material theme
+        docs = pkgs.mkShell {
+          buildInputs = [
+            pkgs.python3Packages.pycairo
+            pkgs.python3Packages.cairosvg
+            pkgs.python3Packages.mkdocs
+            pkgs.python3Packages.mkdocs-material
+            pkgs.python3Packages.mkdocs-material-extensions
+            pkgs.python3Packages.pymdown-extensions
+          ];
+          shellHook = ''
+            python -m venv --system-site-packages .venv
+            source .venv/bin/activate
+            pip install -r docs/requirements.txt
+            echo "Development environment for MkDocs with Material theme is ready."
+            echo "You can run 'mkdocs serve' to start the local server."
+            echo "To build the documentation, run 'mkdocs build'."
+            echo "To deploy the documentation, run 'mkdocs gh-deploy'."
+            echo "To install additional Python packages, use 'pip install <package-name>'."
+          '';
+        };
       };
     };
 }
